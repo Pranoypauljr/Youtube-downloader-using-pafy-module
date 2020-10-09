0da90155dae67ef53d312ec499791b575f4c13a5
PranoyTittu <51205605+PranoyTittu@users.noreply.github.com>
2020-10-09 23:48:51 +0530

    Update youtube video downloader.py
    add some tkinter code to give the miniapp a semipro touch:)

--- youtube video downloader.py
+++ youtube video downloader.py
@@ -1,11 +1,23 @@
+from tkinter import *
 import pafy as pf
-url="<url of youtube video is to be added here>"
-video=pf.new(url)
-streams=video.streams
-for i in streams:
-    print(i)
-best=video.getbest()
-print(best.resolution,best.extension)
-best.download()
-print("success")
-
+app=Tk()
+app.title("youtube downloader")
+app.geometry("300x200")
+app.iconbitmap("yt.ico")
+Label(app,text="Paste the youtube link",font="Arial").pack()
+def api():
+    f=e.get()
+    url=f
+    video=pf.new(url)
+    streams=video.streams
+    for i in streams:
+        print(i)
+    best=video.getbest()
+    print(best.resolution,best.extension)
+    best.download()
+    print("success")       
+e=Entry(app,width=50)
+e.pack()
+photo=PhotoImage("yt.jpg")
+Button(app,text="Download",command=lambda:api()).pack()
+app.mainloop()
